a92b90b65a98cd359ad138eea247210e51c4ee55
Christopher Flowers <cvflowers97@gmail.com>
2019-07-27 23:00:06 -0500

    Movement of character
    Started movement of character with the left joystick of an xbox controller. Need to account for the angular movement still.

---
+++ PhaseBlade/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera mainCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        /** CAMERA INPUT / ROTATION INPUT
+        Debug.Log("Right Horizontal");
+        Debug.Log(Input.GetAxis("Controller Right Horizontal"));
+        Debug.Log("--------------------");
+
+        Debug.Log("Right Vertical");
+        Debug.Log(Input.GetAxis("Controller Right Vertical"));
+        Debug.Log("--------------------");
+        **/
+    }
+}
---
+++ PhaseBlade/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+
+    public float movementSpeed = 5.0f;
+
+    public float horizontalInput = 0.0f;
+    public float verticalInput = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    // Could move code to their own functions.
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Controller Left Horizontal");
+        if (horizontalInput < 0)
+        {
+            rigidBody.transform.Translate(Vector3.left * ( -1 * movementSpeed * horizontalInput) * Time.deltaTime);
+            Debug.Log(movementSpeed * horizontalInput);
+        } else if (horizontalInput > 0)
+        {
+            rigidBody.transform.Translate(Vector3.right * (movementSpeed * horizontalInput) * Time.deltaTime);
+        }
+
+        verticalInput = Input.GetAxis("Controller Left Vertical");
+        if (verticalInput < 0)
+        {
+            rigidBody.transform.Translate(Vector3.back* (-1 * movementSpeed * verticalInput) * Time.deltaTime);
+        } else if (verticalInput > 0)
+        {
+            rigidBody.transform.Translate(Vector3.back * (-1 * movementSpeed * verticalInput) * Time.deltaTime);
+        }
+    }
+}
